27e086bb700461e0a6f2ff228a352a3ad04e3d7c
ahmed el <ahmedelmeskyny@gmail.com>
2021-04-23 21:33:36 +0000

    magensao

---
+++ db/dbMag.js
@@ -0,0 +1,10 @@
+export const  magdb = [
+    {
+        id:1,
+        thumbnail:"/magensao.jpeg",
+        title:"MagEnsao N°1",
+        date:"Avril , 2021",
+        pdf:"https://drive.google.com/file/d/1MQgF-Y7sqZVYZUKhLiewziWHGzT_MG4m/view"
+    },
+
+]
--- pages/magensao.js
+++ pages/magensao.js
@@ -11,8 +11,8 @@ import styles from "../styles/magensao.module.scss";
 //components
 import Layout from "../components/Layout/layout";
 
-
-
+//db
+import {magdb}from "../db/dbMag"
 
 
 
@@ -25,6 +25,9 @@ function About() {
       <Head>
         <title>Alensao || MagEnsao</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="MagEnsao"></meta>
+        <meta property="og:image" content="/og2.png"></meta>
+        <meta property="og:description" content="Un nouveau concept , en collaboration entre l'ALENSAO et l'ADE, d'un magazine bimestriel couvrant les événements au sein de l'ENSAO. Reportages, articles, news... soyez aux aguets pour notre nouvelle revue : MagEnsao!"></meta>
       </Head>
 
       <Layout 
@@ -39,11 +42,26 @@ function About() {
              <p>Accueil / MagEnsao </p>
           </div>
         </div>
-        <div className={styles.building} >
+        <h1 className={styles.title}><div></div>La Premier Magazine de l'ENSAO<div></div></h1>
+        <div className={styles.magContainer}>
+        {
+          magdb.map(
+            (mag)=> 
+            <a href={mag.pdf} target="_blank"><div className={styles.mag}>
+              <div className={styles.thumbnail} style={{backgroundImage:`url(${mag.thumbnail})`}}></div>
+              <div className={styles.text}>
+              <h4>{mag.title}</h4>
+              <p>{mag.date}</p>
+            </div>
+          </div></a> 
+          )
+        }
+        </div>
+        {/* <div className={styles.building} >
           <h1 style={{color:"red"}}>En cours de construction ....</h1>
             <Image alt="construction" src="/building.svg" width="300px" height="300px"></Image>
-            <h3>Un nouveau concept en cours de réalisation, en collaboration entre l'ALENSAO et l'ADE, d'un magazine bimestriel couvrant les événements au sein de l'ENSAO. Reportages, articles, news... soyez aux aguets pour notre nouvelle revue : MagEnsao!</h3>
-        </div>
+            <h3>Un nouveau concept , en collaboration entre l'ALENSAO et l'ADE, d'un magazine bimestriel couvrant les événements au sein de l'ENSAO. Reportages, articles, news... soyez aux aguets pour notre nouvelle revue : MagEnsao!</h3>
+        </div> */}
       </Layout>
 
     </div>
