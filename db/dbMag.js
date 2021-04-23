fd172015e86dd8de30a4833ad2cd7094d9b01137
ahmed el <ahmedelmeskyny@gmail.com>
2021-04-23 21:53:27 +0000

    drive

--- db/dbMag.js
+++ db/dbMag.js
@@ -4,7 +4,7 @@ export const  magdb = [
         thumbnail:"/magensao.jpeg",
         title:"MagEnsao N°1",
         date:"Avril , 2021",
-        pdf:"https://drive.google.com/file/d/1MQgF-Y7sqZVYZUKhLiewziWHGzT_MG4m/view"
+        pdf:"https://drive.google.com/file/d/1MQgF-Y7sqZVYZUKhLiewziWHGzT_MG4m/view?usp=sharing"
     },
 
 ]
