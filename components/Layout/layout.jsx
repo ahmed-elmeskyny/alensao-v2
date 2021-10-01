f52ceeae0debcb3ea6ea45a9ece943835cae9076
ahmed el <ahmedelmeskyny@gmail.com>
2021-10-01 22:49:06 +0100

    magensao

--- components/Layout/layout.jsx
+++ components/Layout/layout.jsx
@@ -1,6 +1,5 @@
 //react
-import {useState} from "react";
-
+import { useState } from "react";
 
 //styles
 import styles from "./layout.module.scss";
@@ -10,39 +9,56 @@ import Menu from "../Menu/Menu";
 import Header from "../Header/header";
 import Footer from "../Footer/footer";
 
-import {HiMenuAlt1} from "react-icons/hi";
-import {MdEmail} from "react-icons/md";
-import {FaFacebookSquare} from "react-icons/fa";
+import { HiMenuAlt1 } from "react-icons/hi";
+import { MdEmail } from "react-icons/md";
+import { FaFacebookSquare } from "react-icons/fa";
 
-import Login from "../Login/login"
+import Login from "../Login/login";
 import Register from "../Register/register";
 
 const Layout = (props) => {
-    const [width , setwidth] = useState("0%");
+  const [width, setwidth] = useState("0%");
 
-    return (
-        <div className={styles.container}> 
-             {/* <div className={styles.adsContainer} >
-                <div className={styles.ads}>
-                    <p>PUBLICITE</p>
-                </div>
-             </div> */}
-             <Header setOpenLogin={props.setOpenLogin} setOpenRegister={props.setOpenRegister}></Header>          
-             <Menu width={width} setwidth={setwidth}></Menu> 
-             <div className={styles.menu}>
-                <HiMenuAlt1 onClick={()=> setwidth("100%")}></HiMenuAlt1>
-                <div className={styles.social}>
-                   <a href="https://web.facebook.com/Association.ALENSAO?_rdc=1&_rdr" target="_blank"> <FaFacebookSquare className={styles.icon}></FaFacebookSquare></a>
-                   <a href="mailto:alensao.association@gmail.com"  target="_blank"> <MdEmail className={styles.icon} style={{fontSize:"22px"}}></MdEmail></a>
-                </div> 
-            </div>
-             {props.children}
-             <Footer></Footer>
-             <Login open={props.openLogin} setOpen={props.setOpenLogin}></Login>
-             <Register open={props.openRegister} setOpen={props.setOpenRegister} ></Register>
+  return (
+    <div className={styles.container}>
+      {/* <div className={styles.adsContainer}>
+        <div className={styles.ads}>
+          <img src="/pub.jpg"></img>
         </div>
-    )
-}
-
+      </div> */}
+      <Header
+        setOpenLogin={props.setOpenLogin}
+        setOpenRegister={props.setOpenRegister}
+      ></Header>
+      <Menu width={width} setwidth={setwidth}></Menu>
+      <div className={styles.menu}>
+        <HiMenuAlt1 onClick={() => setwidth("100%")}></HiMenuAlt1>
+        <div className={styles.social}>
+          <a
+            href="https://web.facebook.com/Association.ALENSAO?_rdc=1&_rdr"
+            target="_blank"
+          >
+            {" "}
+            <FaFacebookSquare className={styles.icon}></FaFacebookSquare>
+          </a>
+          <a href="mailto:alensao.association@gmail.com" target="_blank">
+            {" "}
+            <MdEmail
+              className={styles.icon}
+              style={{ fontSize: "22px" }}
+            ></MdEmail>
+          </a>
+        </div>
+      </div>
+      {props.children}
+      <Footer></Footer>
+      <Login open={props.openLogin} setOpen={props.setOpenLogin}></Login>
+      <Register
+        open={props.openRegister}
+        setOpen={props.setOpenRegister}
+      ></Register>
+    </div>
+  );
+};
 
 export default Layout;
--- db/dbMag.js
+++ db/dbMag.js
@@ -1,19 +1,26 @@
-export const  magdb = [
-    {
-        id:1,
-        thumbnail:"/magensao.jpeg",
-        title:"MagEnsao N°1",
-        date:"Avril , 2021",
-        pdf:"https://drive.google.com/file/d/1MQgF-Y7sqZVYZUKhLiewziWHGzT_MG4m/view?usp=sharing",
-        read:"https://fr.calameo.com/read/0067401467ce527d906f1"
-    },
-    {
-        id:2,
-        thumbnail:"/magensao2.jpg",
-        title:"MagEnsao N°2",
-        date:"Juin , 2021",
-        pdf:"https://drive.google.com/file/d/1iksCefYwet4U-2WLCXJE-7sj3hz63aOz/view?usp=sharing",
-        read:"https://fr.calameo.com/read/0067401463e964ad19129"
-    },
-
-]
+export const magdb = [
+  {
+    id: 1,
+    thumbnail: "/magensao.jpeg",
+    title: "MagEnsao N°1",
+    date: "Avril , 2021",
+    pdf: "https://drive.google.com/file/d/1MQgF-Y7sqZVYZUKhLiewziWHGzT_MG4m/view?usp=sharing",
+    read: "https://fr.calameo.com/read/0067401467ce527d906f1",
+  },
+  {
+    id: 2,
+    thumbnail: "/magensao2.jpg",
+    title: "MagEnsao N°2",
+    date: "Juin , 2021",
+    pdf: "https://drive.google.com/file/d/1iksCefYwet4U-2WLCXJE-7sj3hz63aOz/view?usp=sharing",
+    read: "https://fr.calameo.com/read/0067401463e964ad19129",
+  },
+  {
+    id: 3,
+    thumbnail: "/magensao4.png",
+    title: "MagEnsao N°3",
+    date: "Octobre , 2021",
+    pdf: "https://tinyurl.com/magensao-3-pdf",
+    read: "https://tinyurl.com/magensao-3-read",
+  },
+];
