3943dd7b5e3420cd585f7deb913fcea927468dd0
ahmed el <ahmedelmeskyny@gmail.com>
2021-10-31 17:22:28 +0100

    cards

--- db/conventions.js
+++ db/conventions.js
@@ -4,7 +4,7 @@ export const conventions = [
     title: "Loftin",
     logo: "/loftin.jpeg",
     convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      "•	10% de remise sur la restauration •	Pack Gouter : Crêpe/Gaufre + Boisson Chaude = 26 dh •	Pack Déjeuner : Poutine/Tacos/Sandwich + Soda + Frite : 30 dh",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
@@ -12,8 +12,7 @@ export const conventions = [
     id: 2,
     title: "Sa7bi",
     logo: "/sa7bi.jpeg",
-    convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    convention: " •	10% de remise  ",
     adress: "Rue Chouhada, Oujda 60000",
     map: "",
   },
@@ -21,17 +20,16 @@ export const conventions = [
     id: 3,
     title: "InessBeauty",
     logo: "/inessbeauty.jpeg",
-    convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    convention: " •	20% de réduction ",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
   {
     id: 4,
     title: "Librairie chariqa",
-    logo: "",
+    logo: "/libraire.jpeg",
     convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      "•	10% de réduction sur les polycopiés •	50% de réduction sur la récupération de données sur PC/USB",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
@@ -39,8 +37,7 @@ export const conventions = [
     id: 5,
     title: "Mr.Smith",
     logo: "/smith.jpeg",
-    convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    convention: " •	30% de réduction sur la restauration",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
@@ -48,8 +45,7 @@ export const conventions = [
     id: 6,
     title: "Sami School of Music",
     logo: "/sami.jpeg",
-    convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    convention: " •	10% de remise sur l’abonnement mensuel",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
@@ -57,8 +53,7 @@ export const conventions = [
     id: 7,
     title: "Milli Gusto",
     logo: "/milli.jpg",
-    convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    convention: " •	20% de remise sur le ticket (sauf Soda ou Café)",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
@@ -66,8 +61,7 @@ export const conventions = [
     id: 8,
     title: "Para El meskyny",
     logo: "/parapharmacie.jpg",
-    convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    convention: " •	15% de remise sur les produits parapharmacie",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
@@ -75,8 +69,7 @@ export const conventions = [
     id: 9,
     title: "Opticien Lehlou",
     logo: "/lehlou.jpg",
-    convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    convention: " •	Test visuel gratuit •	20% de remise ",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
@@ -84,8 +77,7 @@ export const conventions = [
     id: 10,
     title: "Lunaria",
     logo: "/lunaria.jpeg",
-    convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    convention: " •	Soda offert pour tout tacos, burger ou sandwich",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
@@ -93,27 +85,34 @@ export const conventions = [
   {
     id: 11,
     title: "Gym Corsica",
-    logo: "",
-    convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    logo: "/gym2.jpeg",
+    convention: " •	25% de réduction pour chaque groupe de 10 personnes",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
   {
     id: 11,
     title: "Gym Men Fit",
-    logo: "",
-    convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    logo: "/gym.jpeg",
+    convention: "•	25% de réduction pour chaque groupe de 10 personnes",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
   {
     id: 12,
     title: "City play",
-    logo: "",
+    logo: "/cityplay.jpeg",
     convention:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      " 10h => 15h : partie jouée = partie offerte Petit Snooker : 7 parties = 100dh/ 14 parties = 200dh/ 22 parties = 300dh Grand Snooker : 7 parties pour 150dh/ 14 parties = 300dh/ 22 parties = 400dh PS4 :7h = 100dh/ 14h = 200dh/ 21h = 300dh 8Ball Pool :15 parties = 50dh/ 33 parties = 100dh/ 47 parties = 150dh",
+    adress: "Rue Miraj، Oujda 60000",
+    map: "",
+  },
+  {
+    id: 13,
+    title: "afaf",
+    logo: "/afaf.jpeg",
+    convention:
+      " •	Pack 1 : Sandwich (dinde et fromage) + Salade + Boisson à 32DH •	Pack 2 : ¼ Poulet + riz + boisson à 30DH",
     adress: "Rue Miraj، Oujda 60000",
     map: "",
   },
